9ab7d2e915aefd78dbf59f4cf81e40bb6ae0f7a6
tharunkorla <141128952+tharunkorla@users.noreply.github.com>
2023-08-01 14:47:52 +0530

    Create vote.c

---
+++ vote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("enter the age");
+	scanf("%d",&age);
+	if (age>=18)
+	printf("you can vote");
+	else
+	printf("you cannot vote");
+	return 0;
+}
