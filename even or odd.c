5e0e6a759045dbd6088d6a174c63453e8c8d3c79
tharunkorla <141128952+tharunkorla@users.noreply.github.com>
2023-08-02 13:57:46 +0530

    Update even or odd.c

--- even or odd.c
+++ even or odd.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-int main() {
-    int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    if(num % 2 == 0)
-        printf("%d is even.", num);
-    else
-        printf("%d is odd.", num);
-    
-    return 0;
+int main() 
+{
+    int n;
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+ 
+    int arr[n];
+    printf("Enter %d elements in the array: ",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        
+        
+    }
+    printf("Even numbers in the array are: ");
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+            printf("%d ", arr[i]);
+    }
+    printf("\nOdd numbers in the array are: ");
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==1)
+            printf("%d ", arr[i]);
+    }
+    return 0;
 }
